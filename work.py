73b4e1f621279d741bc9bc537a4efe30ef242cba
sxgwork <sxg@163.com>
2016-10-09 20:57:08 +0800

    2016-10-9 workpc

--- work.py
+++ work.py
@@ -1,4 +1,14 @@
-name=input('input your name:')
-print('Hello',name)
-print('\\\t\\\\')
-print(r'\\\t\\\\')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('*********begin debug***********')
+s1 = 72
+s2 = 85
+r = (s2 - s1) / s1 * 100
+print(r)
+print('xiaoming\'s score improve %.2f%%' % r)
+#%.2f%%',r
+#name=input('input your name:')
+#print('Hello',name)
+#print('\\\t\\\\')
+#print(r'\\\t\\\\')
+print('*********end debug***********')
